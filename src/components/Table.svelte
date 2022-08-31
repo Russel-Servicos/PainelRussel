5e14d05d03da2ee72f7b603972bda992a2a6b0c7
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-31 15:39:59 -0300

    Mocka dados para serem apresentados na tabela

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -6,6 +6,50 @@
 	import Menu from '@smui/menu';
 	import List, { Item, Separator, Text } from '@smui/list';
 
+
+	let options = [
+		{
+			code: '123123123312',
+			date: '22/02/1994',
+			client: 'Gean Ferreira',
+			paymentType: 'Cartão de crédito',
+			status: 'Aguardando pagamento',
+			total: 20000
+		},
+		{
+			code: '123123123312',
+			date: '22/02/1994',
+			client: 'Arthur Fusco',
+			paymentType: 'Cartão de crédito',
+			status: 'Aguardando pagamento',
+			total: 20000
+		},
+		{
+			code: '123123123312',
+			date: '22/02/1994',
+			client: 'Pedro Henrique',
+			paymentType: 'Cartão de crédito',
+			status: 'Aguardando pagamento',
+			total: 20000
+		},
+		{
+			code: '123123123312',
+			date: '22/02/1994',
+			client: 'João',
+			paymentType: 'Cartão de crédito',
+			status: 'Aguardando pagamento',
+			total: 20000
+		},
+		{
+			code: '123123123312',
+			date: '22/02/1994',
+			client: 'Alasca',
+			paymentType: 'Cartão de crédito',
+			status: 'Aguardando pagamento',
+			total: 20000
+		}
+	];
+
 </script>
 
 <main style=" text-align: end; padding: 0 24px;">
@@ -47,6 +91,24 @@
 			</Row>
 		</Head>
 		<Body>
+			{#each options as option (option.client)}
+				<Row>
+					<Cell checkbox>
+						<Checkbox bind:group={selected} value={option} valueKey={option.client} />
+					</Cell>
+					<Cell>{option.code}</Cell>
+					<Cell>{option.date}</Cell>
+					<Cell>{option.client}</Cell>
+					<Cell>{option.paymentType}</Cell>
+					<Cell>{option.status}</Cell>
+					<Cell>{option.total}</Cell>
+					<Cell
+						><Button color="secondary" on:click={() => console.log('funcionei')} variant="outlined">
+							<Label>MAIS DETALHES</Label>
+						</Button></Cell
+					>
+				</Row>
+			{/each}
 		</Body>
 	</DataTable>
 </main>
